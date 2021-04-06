92e2fdce8a17555a517a0adaf9b27083cead5274
Dennis Collon <dennis.collon@secucloud.com>
2021-04-06 12:54:16 +0200

    add caris

--- src/components/TrainerAvatar/TrainerAvatar.jsx
+++ src/components/TrainerAvatar/TrainerAvatar.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const TrainerAvatar = ({ src, alt }) => (
-  <img width='100%' src={src} alt={alt} />
-);
+const Placeholder = require('../../assets/trainerImages/placeholder.png');
+
+const TrainerAvatar = ({ src, alt }) => {
+  if (!src) return <img width='100%' src={Placeholder} alt={alt} />;
+  return <img width='100%' src={src} alt={alt} />;
+};
 
 export default React.memo(TrainerAvatar);
